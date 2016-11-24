3117e4241eb61bc7285d4712ff2736d8cbb3972c
vlad <vladmyh@gmail.com>
2016-11-24 12:42:20 +0200

    Initial commit

---
+++ src/main/java/com/newsstand/servlets/LoginServlet.java
@@ -0,0 +1,20 @@
+package com.newsstand.servlets;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+	private static final Logger logger = Logger.getLogger(LoginServlet.class);
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		logger.info("LoginServlet");
+		response.sendRedirect("/index.jsp");
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Newsstand</title>
+</head>
+<body>
+    <h1>Main page</h1>
+</body>
+</html>
