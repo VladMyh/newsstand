47f4388c4093f54ef3a06424356a108edcc87445
Vlad Mihajlneko <vladmyh@gmail.com>
2016-12-24 12:15:42 +0200

    updated db schema

--- dbdump.sql
+++ dbdump.sql
@@ -16,6 +16,22 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `magazine`
 --
 
@@ -24,13 +40,32 @@ DROP TABLE IF EXISTS `magazine`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `magazine` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(30) DEFAULT NULL,
-  `publisherId` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `quantity` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(4,2) DEFAULT NULL,
+  `publisherId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_publisherId` (`publisherId`),
-  CONSTRAINT `magazine_ibfk_1` FOREIGN KEY (`publisherId`) REFERENCES `publisher` (`id`) ON UPDATE CASCADE
+  KEY `fk_magazine_1_idx` (`publisherId`),
+  CONSTRAINT `fk_magazine_1` FOREIGN KEY (`publisherId`) REFERENCES `publisher` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `magazineTags`
+--
+
+DROP TABLE IF EXISTS `magazineTags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `magazineTags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `magazineId` int(11) NOT NULL,
+  `tagId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_magazineTags_1_idx` (`magazineId`),
+  KEY `fk_magazineTags_2_idx` (`tagId`),
+  CONSTRAINT `fk_magazineTags_1` FOREIGN KEY (`magazineId`) REFERENCES `magazine` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_magazineTags_2` FOREIGN KEY (`tagId`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,7 +78,46 @@ DROP TABLE IF EXISTS `publisher`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `publisher` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subscription`
+--
+
+DROP TABLE IF EXISTS `subscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subscription` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `magazineId` int(11) NOT NULL,
+  `subscriptionTypeId` int(11) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_subscription_1_idx` (`userId`),
+  KEY `fk_subscription_2_idx` (`magazineId`),
+  KEY `fk_subscription_3_idx` (`subscriptionTypeId`),
+  CONSTRAINT `fk_subscription_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscription_2` FOREIGN KEY (`magazineId`) REFERENCES `magazine` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscription_3` FOREIGN KEY (`subscriptionTypeId`) REFERENCES `subscriptionType` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subscriptionType`
+--
+
+DROP TABLE IF EXISTS `subscriptionType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subscriptionType` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -57,9 +131,27 @@ DROP TABLE IF EXISTS `tag`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(25) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -71,4 +163,4 @@ CREATE TABLE `tag` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-07 23:52:46
+-- Dump completed on 2016-12-24 12:14:49
